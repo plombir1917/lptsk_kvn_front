2a6793129c36173f0edb63e17b3134606c24c941
plombir1917 <dfhhggh@list.ru>
2024-05-18 14:58:19 +0300

    feat: landing structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ['@nuxtjs/color-mode', '@nuxt/image'],
+  css: ['~/assets/css/app.css'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
