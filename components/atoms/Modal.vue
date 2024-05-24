9e7772ebb7ca9ae1d29b3cf6124a7f69fbc7c768
KopichZS <dfhhggh@list.ru>
2024-05-24 16:28:21 +0300

    feat: video adaptive

--- components/atoms/Modal.vue
+++ components/atoms/Modal.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import type { Ref } from 'vue';
 
 const videoUrl: string =
-  'https://vk.com/video_ext.php?oid=-56052247&id=456239806&hd=3&autoplay=1';
+  'https://vk.com/video_ext.php?oid=-56052247&id=456239806&hd=3';
 
 const isModalOpen: Ref<boolean> = ref(false);
 
@@ -17,55 +17,61 @@ function closeModal() {
 </script>
 
 <template>
-  <div>
-    <AtomsLinkBtn
-      href="#"
-      @click="openModal"
-      variant="costum"
-      costum-span-class="px-6 md:px-7 hover:bg-primary/10"
-      costum-text-color="text-blue-500"
-    >
-      <div class="flex items-center gap-3">
-        <span class="p-2 rounded-full border border-blue-500">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-3 h-3"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </span>
-
-        <span> Смотреть видео </span>
-      </div>
-    </AtomsLinkBtn>
-
-    <Teleport to="body">
-      <div
-        v-if="isModalOpen"
-        class="fixed z-50 inset-0 flex items-center justify-center"
+  <transition name="modal">
+    <div>
+      <AtomsLinkBtn
+        href="#"
+        @click="openModal"
+        variant="costum"
+        costum-span-class="px-6 md:px-7 hover:bg-primary/10"
+        costum-text-color="text-blue-500"
       >
-        <div
-          @click="closeModal"
-          class="absolute inset-0 bg-gray-900 opacity-50 z-40"
-        ></div>
+        <div class="flex items-center gap-3">
+          <span class="p-2 rounded-full border border-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-3 h-3"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
 
-        <iframe
-          :src="videoUrl"
-          title="VK video player"
-          frameborder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
-          class="absolute h-3/4 w-3/4 max-w-full max-h-full z-50"
-        ></iframe>
-      </div>
-    </Teleport>
-  </div>
+          <span> Смотреть видео </span>
+        </div>
+      </AtomsLinkBtn>
+
+      <Teleport to="body">
+        <div
+          v-if="isModalOpen"
+          class="fixed z-50 inset-0 flex items-center justify-center p-4"
+        >
+          <div
+            @click="closeModal"
+            class="absolute inset-0 bg-gray-900 opacity-50 z-40"
+          ></div>
+
+          <div
+            class="relative z-50 bg-gray-700 p-1 rounded-lg max-w-screen-lg w-full h-auto md:w-3/4 md:h-3/4 overflow-hidden"
+          >
+            <iframe
+              :src="videoUrl"
+              title="VK video player"
+              frameborder="0"
+              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; autoplay; fullscreen; picture-in-picture"
+              allowfullscreen
+              class="w-full h-64 md:h-full"
+            ></iframe>
+          </div>
+        </div>
+      </Teleport>
+    </div>
+  </transition>
 </template>
