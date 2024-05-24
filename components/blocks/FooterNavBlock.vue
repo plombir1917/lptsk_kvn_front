859c8be0f8a03514b3db056b76cf6d69f22fa9ad
KopichZS <dfhhggh@list.ru>
2024-05-24 16:27:37 +0300

    fix: footer hover, tw link

--- components/blocks/FooterNavBlock.vue
+++ components/blocks/FooterNavBlock.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="">
-        <h2 class="text-xl font-bold text-gray-700 dark:text-white">
-            {{ title }}
-        </h2>
-        <ul class="pt-6 space-y-3">
-            <li v-for="link in links">
-                <NuxtLink :to="link.href" class="transition text-gray-600 dark:text-gray-300 hover:!text-primary">
-                    {{ link.text }}
-                </NuxtLink>
-            </li>
-        </ul>
-    </div>
+  <div class="">
+    <h2 class="text-xl font-bold text-gray-700 dark:text-white">
+      {{ title }}
+    </h2>
+    <ul class="pt-6 space-y-3">
+      <li v-for="link in links">
+        <NuxtLink
+          :to="link.href"
+          class="transition text-gray-600 dark:text-gray-300 hover:!text-blue-500"
+        >
+          {{ link.text }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
 const { title, links } = defineProps<{
-    title: string,
-    links: { text: string, href: string }[]
-}>()
+  title: string;
+  links: { text: string; href: string }[];
+}>();
 </script>
