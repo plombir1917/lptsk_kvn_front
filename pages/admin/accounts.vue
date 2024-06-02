0b3b186a46c115ac7ce1a70308ff563ca191a1d4
plombir1917 <dfhhggh@list.ru>
2024-06-02 15:52:05 +0300

    feat: "users" page to "accounts"

--- pages/admin/accounts.vue
+++ pages/admin/accounts.vue
@@ -13,7 +13,17 @@
             <th
               class="py-3 px-6 bg-gray-200 dark:bg-gray-700 text-left text-xs font-medium text-gray-600 dark:text-gray-300 uppercase tracking-wider"
             >
+              Фото
+            </th>
+            <th
+              class="py-3 px-6 bg-gray-200 dark:bg-gray-700 text-left text-xs font-medium text-gray-600 dark:text-gray-300 uppercase tracking-wider"
+            >
               Имя
+            </th>
+            <th
+              class="py-3 px-6 bg-gray-200 dark:bg-gray-700 text-left text-xs font-medium text-gray-600 dark:text-gray-300 uppercase tracking-wider"
+            >
+              Фамилия
             </th>
             <th
               class="py-3 px-6 bg-gray-200 dark:bg-gray-700 text-left text-xs font-medium text-gray-600 dark:text-gray-300 uppercase tracking-wider"
@@ -34,21 +44,27 @@
         </thead>
         <tbody>
           <tr
-            v-for="user in users"
-            :key="user.id"
-            class="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700"
+            v-for="account in accounts"
+            :key="account.id"
+            class="bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-600 dark:text-gray-300"
           >
             <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
-              {{ user.name }}
+              {{ account.photo }}
             </td>
             <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
-              {{ user.phone }}
+              {{ account.name }}
             </td>
             <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
-              {{ user.login }}
+              {{ account.surname }}
             </td>
             <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
-              {{ user.role }}
+              {{ account.phone }}
+            </td>
+            <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
+              {{ account.login }}
+            </td>
+            <td class="py-4 px-6 border-b border-gray-200 dark:border-gray-700">
+              {{ account.role }}
             </td>
           </tr>
         </tbody>
@@ -59,22 +75,50 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
 
-const users = ref([]);
+const accounts = ref([]);
 
-const fetchUsers = async () => {
-  try {
-    // const response = await axios.get('/api/users'); // URL вашего API
-    users.value = response.data;
-  } catch (error) {
-    console.error('Error fetching users:', error);
+async function fetchAccounts() {
+  const query = `
+  query {
+  getAccounts {
+    name
+    surname
+    phone
+    login
+    role
+    photo
   }
-};
+}
 
-onMounted(fetchUsers);
+  `;
+  try {
+    const token = localStorage.getItem('access_token');
+    const response = await fetch('http://localhost:3001/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    const result = await response.json();
+    if (response.ok && result.data && result.data.getAccounts) {
+      accounts.value = result.data.getAccounts;
+    } else {
+      console.error('Fetching profile failed:', result.errors);
+      alert('Fetching profile failed. Please try again.');
+    }
+  } catch (error) {
+    console.error('Error fetching accounts:', error);
+  }
+}
+
+onMounted(fetchAccounts);
 
 definePageMeta({
   layout: 'admin',
+  middleware: 'auth',
 });
 </script>
